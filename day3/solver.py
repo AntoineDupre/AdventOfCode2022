f7e572f51d129c9db5977ed93ddb0ec60b26f5ab
AntoineDupre <dupre.antoine.nicolas@gmail.com>
2022-12-04 17:36:44 +0100

    Day3 part2

--- day3/solver.py
+++ day3/solver.py
@@ -2,6 +2,14 @@
 with open("dataset", "r") as fp:
     data = fp.readlines()
 
+data = list(map(str.strip, data))
+
+
+def compute_prio(letter):
+    return (
+        ord(letter) - lower_offset if letter.islower() else ord(letter) - upper_offset
+    )
+
 
 # Part 1
 
@@ -11,13 +19,18 @@ mistakes = [
     set(set(d[: int(len(d) / 2)]) & set(d[int(len(d) / 2) :])).pop() for d in data
 ]
 
-error = sum(
-    [
-        ord(mistake) - lower_offset
-        if mistake.islower()
-        else ord(mistake) - upper_offset
-        for mistake in mistakes
-    ]
-)
+error = sum((compute_prio(mistake) for mistake in mistakes))
+print(error)
 
 # Part 2
+
+def group_iterator():
+    data_len = len(data)
+    index = 0
+    while index < data_len:
+        yield data[index], data[index + 1], data[index + 2]
+        index += 3
+
+
+print(sum(compute_prio(set(set(x) &set(y) & set(z)).pop()) for x, y, z in group_iterator()))
+
